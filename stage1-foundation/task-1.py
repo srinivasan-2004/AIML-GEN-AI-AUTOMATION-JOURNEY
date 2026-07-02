9104a96a9e8a4d3b1c12ed73f25d0818a34d1fe6
srinivasan-2004 <srinisenu417@gmail.com>
2026-07-02 09:12:26 +0530

    Task 1 - syntax and First Program

---
+++ stage1-foundation/task-1.py
@@ -0,0 +1,5 @@
+# write a python script that prints your name,age and city. use print(),string formatting and comments and run it.
+name = "Srinivasan"
+age = 22
+city = "Neyveli"
+print(f" my name is {name}, i am { age} years old and i live in{city} city.")
