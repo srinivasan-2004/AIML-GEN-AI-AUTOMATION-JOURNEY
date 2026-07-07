b84c0481f213395d9bd35ec7964ab7c0aa545ed6
srinivasan-2004 <srinisenu417@gmail.com>
2026-07-07 07:59:03 +0530

    Tak-2Variables and Data Types

---
+++ stage1-foundation/task-2.py
@@ -0,0 +1,16 @@
+#Create variables of every type: int, float, str, bool. Print each. Print the type of each using type(). Convert between types using int(), float(),
+# values
+age=22
+name="SRINIVASAN"
+height=5.9
+is_student=True
+# typeof the vales
+print(type(age))
+print(type(name))
+print(type(height))
+print(type(is_student))
+#type conversion for the above values
+print(float(age))
+print(bool(name))
+print(str(height))
+
